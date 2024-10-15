046de99b7d60482344241f76637daadfed04b518
brandon-cury <moukambrandoncury@gmail.com>
2024-10-15 15:57:54 +0200

    first commit

---
+++ assets/vue/controllers/Hello.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>Hello {{ name }} {{ prenom }}!</div>
+</template>
+
+<script setup>
+import {onMounted, ref} from "vue";
+
+    defineProps({
+        name: String
+    });
+    const prenom = ref("Brandon");
+    onMounted(()=>{
+          alert('bebeb');
+          console.log('dddddddddddd')
+        }
+
+    )
+</script>
