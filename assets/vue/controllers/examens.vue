019fda368b9625d42cfdbff10ec8b2d2fd1b6ced
brandon-cury <moukambrandoncury@gmail.com>
2024-10-23 13:34:58 +0200

    add filiere

--- assets/vue/controllers/examens.vue
+++ assets/vue/controllers/examens.vue
@@ -2,7 +2,6 @@
 
     <section class="examensSlider slider">
       <ExamenCard v-for="examen in examens" :examen="examen" />
-
     </section>
 
 
@@ -23,53 +22,37 @@ onMounted(()=>{
     method: 'GET',
     success: (response) => {
        examens.value = response;
-       isVisible.value = true;
        setTimeout(()=>{
          $('.examensSlider').slick({
            dots: false,
            infinite: true,
            speed: 300,
-           slidesToShow: 1,
+           slidesToShow: 6,
            centerMode: true,
            variableWidth: true,
-           arrows: false,
+           arrows: true,
+           responsive: [
+             {
+               breakpoint: 993,
+               settings: {
+                 dots: false,
+                 infinite: true,
+                 speed: 300,
+                 slidesToScroll: 4,
+                 centerMode: true,
+                 variableWidth: true,
+                 arrows: false,
+               }
+             },
+           ]
          });
        }, 100);
-
-      /*$('.examensSlider').slick({
-        dots: false,
-        infinite: true,
-        speed: 300,
-        slidesToShow: 1,
-        centerMode: true,
-        variableWidth: true,
-        arrows: false,
-      });*/
-      //isVisible.value = true;
-      //activeSlider();
     },
     error: (error) => {
       console.error(error);
     }
   });
 });
-const activeSlider = ()=>{
-  alert('dddAA');
-  $('.examensSlider').slick({
-    dots: false,
-    infinite: true,
-    speed: 300,
-    slidesToShow: 1,
-    centerMode: true,
-    variableWidth: true,
-    arrows: false,
-  });
-  if(isVisible.value == true){
-    $(document).on('ready', function() {
-
-    });
-  }
-};
 
 
 
@@ -77,59 +60,6 @@ const activeSlider = ()=>{
 </script>
 <style scoped>
 
-* {
-  box-sizing: border-box;
-}
-.slider {
-  bottom: 50px;
-  width: 100%;
-}
-
-.slick-slide {
-  margin: 0px 20px;
-}
-
-.slick-slide img {
-  width: 100%;
-}
-
-.slick-prev:before,
-.slick-next:before {
-  color: black;
-}
-
-.slick-slide {
-  transition: all ease-in-out .3s;
-  opacity: .2;
-}
-
-.slick-active {
-  opacity: .5;
-}
-
-.slick-current {
-  opacity: 1;
-}
-.card{
-  width: 250px !important;
-  text-decoration: none;
-  margin-left: 30px;
-}
-.card .imgInfoox{
-  position: relative;
-  width: 80%;
-  height: 150px;
-  overflow: hidden;
-  margin: 10px auto;
-}
-.card .imgInfoox img{
-  width:  100%;
-  height: auto;
-  max-height: 150px;
-}
-.card .card-body .card-title{
-  color:#E31524;
-}
 /* Tablette */
 @media screen and (min-width: 768px) {
 
